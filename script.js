39900ab5eefe2e599cf4e9e823ccd14ca9c56fd1
colinnnnnnnnnnn <kalinpoiata@gmail.com>
2023-06-29 17:54:52 +0300

    Plus button shows form, submit button hides it

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let myLibrary = [];
 
 const form = document.getElementById('form');
-form.addEventListener('submit', newBook);
+const formContainer = document.querySelector('.form-container');
+const button = document.getElementById('add');
+
+button.addEventListener('click', () => {
+    formContainer.style.display = 'flex';
+});
+
+form.addEventListener('submit', addBook);
 
 function Book(title, author, read) {
     this.title = title;
@@ -9,13 +16,17 @@ function Book(title, author, read) {
     this.read = read;
 }
 
-function newBook(event) {
+function addBook(event) {
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let read = document.getElementById('yes').checked;
     let book = new Book(title, author, read);
     myLibrary.push(book);
 
+    console.log(myLibrary[0]);
+
+    form.reset();
+    formContainer.style.display = 'none';
     event.preventDefault();
 }
 
