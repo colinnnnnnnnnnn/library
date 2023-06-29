215e0624fffe7ec3ddbcfa1ea8874de5ea331cdd
colinnnnnnnnnnn <kalinpoiata@gmail.com>
2023-06-29 17:37:01 +0300

    Add script for creating a book object and adding it to an array when submiting the form

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let myLibrary = [];
 
-function showForm() {
-    
+const form = document.getElementById('form');
+form.addEventListener('submit', newBook);
+
+function Book(title, author, read) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
 }
 
-function Book() {
+function newBook(event) {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let read = document.getElementById('yes').checked;
+    let book = new Book(title, author, read);
+    myLibrary.push(book);
 
+    event.preventDefault();
 }
+
+
